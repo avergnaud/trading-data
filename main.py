b8a80836fcf5c7333a6ecc726ea16a868ac4991a
Chewby79 <45800920+Chewby79@users.noreply.github.com>
2021-11-29 19:55:57 +0100

    mise en place d'un cron

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+import cron.binance_cron
 from persistence import exchanges_dao, pairs_dao, intervales_dao, ohlc_definition_dao
 
 app = Flask(__name__)
@@ -45,6 +47,12 @@ def ohlc_definitions():
         liste = ohlc_definition_dao.get_all()
         return jsonify(liste)
 
+
+# /launch_cron/:exchange_name
+@app.route("/launch_cron/<exchange>")
+def launch_cron(exchange):
+    result = cron.binance_cron.launch(exchange)
+    return jsonify(result), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
